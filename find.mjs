0719f584a26f5af9b9de427e4c7b325190970b3d
niallus <niallus@gmail.com>
2023-05-29 21:54:41 +0000

    init

---
+++ find.mjs
@@ -0,0 +1,30 @@
+import {promises as fs} from 'fs';
+import prompt from 'prompt';
+
+const schema = {
+  properties: {
+    position: {
+      pattern: /^[0-9]+$/,
+      message: 'You can only enter in numbers fool',
+      required: true
+    }
+  }
+}
+
+prompt.get(schema, async function (err, result) {
+  if (err) {
+    console.error(err);
+  }
+
+   await getMovie(result.position);
+});
+
+const getMovie = async (position) => {
+  const data = await fs.readFile('movies.json');
+  const movies = JSON.parse(data);
+
+  // console.log(movies);
+
+  const movie = movies.list.find(x => x.position === `${position}.`);
+  console.log(movie);
+}
